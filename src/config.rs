ad15a705531e4f0c5f32a0cfe7f0691a8e1ea214
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-08-26 06:15:48 +0900

    Bug fixes

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,5 @@
 use failure::{format_err, Fallible};
+use log::*;
 use std::{
     collections::HashMap,
     fs,
@@ -9,14 +10,14 @@ use std::{
 #[derive(serde::Deserialize)]
 struct CPort {
     image: String,
-    apt: Vec<String>,
+    apt: Option<Vec<String>>,
 }
 
 #[derive(serde::Deserialize)]
 struct CMake {
     generator: Option<String>,
     build: Option<String>,
-    option: HashMap<String, String>,
+    option: Option<HashMap<String, String>>,
 }
 
 #[derive(serde::Deserialize)]
@@ -54,7 +55,10 @@ impl ParsedConfigure {
         f.read_to_string(&mut buf)?;
         let mut cfg: ParsedConfigure = toml::from_str(&buf)?;
         if cfg.source.is_none() {
-            cfg.source = filename.parent().map(|p| p.into());
+            let abspath = filename.canonicalize().map_err(|_| {
+                format_err!("Cannot canonicalize TOML path: {}", filename.display())
+            })?;
+            cfg.source = abspath.parent().map(|p| p.into());
         }
         Ok(cfg)
     }
@@ -64,11 +68,11 @@ impl ParsedConfigure {
             source: self.source.unwrap(),
             // cport
             image: self.cport.image,
-            apt: self.cport.apt,
+            apt: self.cport.apt.unwrap_or(Vec::new()),
             // cmake
             generator: self.cmake.generator.unwrap_or("Ninja".into()),
             build: self.cmake.build.unwrap_or("_cport".into()),
-            option: self.cmake.option,
+            option: self.cmake.option.unwrap_or(HashMap::new()),
         }
     }
 }
@@ -77,7 +81,9 @@ pub fn read_toml<P: AsRef<Path>>(filename: P) -> Fallible<Configure> {
     let filename = filename.as_ref();
     let cfg = ParsedConfigure::load(&filename)
         .map_err(|_| format_err!("Cannot read TOML file: {}", filename.display()))?;
-    Ok(cfg.normalize())
+    let cfg = cfg.normalize();
+    info!("Load {}: {:?}", filename.display(), cfg);
+    Ok(cfg)
 }
 
 #[cfg(test)]
@@ -89,15 +95,15 @@ mod tests {
         let cfg = super::ParsedConfigure::load("cport.toml")?;
 
         assert_eq!(cfg.cport.image, "debian");
-        assert_eq!(cfg.cport.apt, vec!["libboost-dev".to_string()]);
+        assert_eq!(cfg.cport.apt, Some(vec!["libboost-dev".to_string()]));
 
         assert_eq!(cfg.cmake.generator, Some("Ninja".into()));
         assert_eq!(cfg.cmake.build, Some("_cport".into()));
         assert_eq!(
             cfg.cmake.option,
-            hashmap! {
+            Some(hashmap! {
                 "CMAKE_EXPORT_COMPILE_COMMANDS".into() => "ON".into(),
-            }
+            })
         );
         Ok(())
     }
